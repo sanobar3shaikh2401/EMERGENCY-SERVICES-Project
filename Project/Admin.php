a7811c99b97c443b7eb9c09a057733a7580ef719
sanobarshaikh2401 <sanobar24shaikh@gmail.com>
2021-01-08 16:47:07 +0530

    emergency services

---
+++ Project/Admin.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html>
+<head>
+<link href="index.css" rel="stylesheet" type="text/css">
+<title>Admin contact</title>
+</head>
+
+<body>
+
+<center><h1>Admin</h1></center>
+
+&nbsp 
+<header>
+
+<ul>
+<li> <a href="#HOME" class="active"><i>HOME</i></a></li>
+</ul>
+
+</header>
+
+&nbsp
+&nbsp
+
+<footer>
+<h2><a href="contact.php">Contact Us</a></h2>
+&nbsp
+&nbsp
+&nbsp
+&nbsp
+</footer>
+
+
+
+</body>
+</html>
